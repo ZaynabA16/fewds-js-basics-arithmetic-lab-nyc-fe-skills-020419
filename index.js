7b5cfb0811453f942697b096ca609ae28a19fb14
Zainab Agboola <zagboola@nyit.edu>
2019-03-11 22:59:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const newID= oldID + 1000000000;
 Number.isInteger(currentAge);
 let ageIsValid = true;
-
 const randomNumber = Math.floor(Math.random() * 20)
 const randomInteger= Math.floor(randomNumber);
 const randomUserID= randomInteger+ 1000000000;
